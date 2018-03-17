421c9f8a52c5f84fad9f864381239d3e311f2315
ziyi <xiajinlei@foxmail.com>
2018-03-17 15:18:14 +0800

    add database and configs of table

---
+++ app/src/main/java/com/guoguoclimate/android/db/Country.java
@@ -0,0 +1,46 @@
+package com.guoguoclimate.android.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by ziyi on 2018/3/17.
+ */
+
+public class Country extends DataSupport {
+    private int id;
+    private String countryName;
+    private String weatherId;
+    private int cityId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountryName() {
+        return countryName;
+    }
+
+    public void setCountryName(String countryName) {
+        this.countryName = countryName;
+    }
+
+    public String getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(String weatherId) {
+        this.weatherId = weatherId;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+}
